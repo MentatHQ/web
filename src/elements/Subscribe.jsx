d147392e2243c3e5e358a9583d620f90bd776e11
Samuel Corso <sam@mentat.org>
2018-09-12 14:21:39 -0400

    updated subscribe form

--- src/elements/Subscribe.jsx
+++ src/elements/Subscribe.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 class Subscribe extends React.Component {
   constructor() {
@@ -9,12 +9,12 @@ class Subscribe extends React.Component {
     };
   }
 
-  async subscribe(email='') {
+  async subscribe(email = "") {
     try {
       if (!email.match(/.@[a-z]{2,}/)) {
-        throw new Error('Please enter a valid email.');
+        throw new Error("Please enter a valid email.");
       }
-      await axios.post('/subscribe', { email });
+      await axios.post("/subscribe", { email });
       this.setState({ subscribed: 1 });
     } catch (e) {
       if (e.message.match(/409/gi)) {
@@ -28,20 +28,37 @@ class Subscribe extends React.Component {
   render() {
     return (
       <div className="Subscribe">
-        { this.state.subscribed === 2 ?
-          <div className="Thanks">You are already subscribed.</div>:
-          this.state.subscribed === 1 ?
-            <div className="Thanks">You are now subscribed.</div>:
-            <form onSubmit={(e) => {
-              e.preventDefault();
-              this.subscribe(this.state.email);
-            }}>
-              <input placeholder="Subscribe with your email" onChange={e => this.setState({ email: e.target.value })} />
-              <button type="submit" title="Subscribe">
-                <span className="icon-send-plane" />
-              </button>
-            </form>
-        }
+        <div id="mc_embed_signup">
+          <form
+            action="https://mentat.us18.list-manage.com/subscribe/post?u=39a9a73f63305a63b0633f092&amp;id=e5bd622ff3"
+            method="post"
+            id="mc-embedded-subscribe-form"
+            name="mc-embedded-subscribe-form"
+            class="validate"
+            target="_blank"
+            novalidate
+          >
+            <div id="mc_embed_signup_scroll">
+              <input
+                type="email"
+                name="EMAIL"
+                class="email"
+                id="mce-EMAIL"
+                placeholder="Enter your email address..."
+                required
+              />
+              <div class="clear">
+                <input
+                  type="submit"
+                  value="Subscribe"
+                  name="subscribe"
+                  id="mc-embedded-subscribe"
+                  class="button"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
