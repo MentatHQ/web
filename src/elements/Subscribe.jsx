d076b2e076902170a00f0265f8295180097b8d4b
Samuel Corso <sam@mentat.org>
2018-09-19 17:06:34 -0400

    Edited subscription form

--- src/elements/Subscribe.jsx
+++ src/elements/Subscribe.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 
 class Subscribe extends React.Component {
   constructor() {
@@ -9,55 +8,26 @@ class Subscribe extends React.Component {
     };
   }
 
-  async subscribe(email = "") {
-    try {
-      if (!email.match(/.@[a-z]{2,}/)) {
-        throw new Error("Please enter a valid email.");
-      }
-      await axios.post("/subscribe", { email });
-      this.setState({ subscribed: 1 });
-    } catch (e) {
-      if (e.message.match(/409/gi)) {
-        this.setState({ subscribed: 2 });
-      } else {
-        alert(e.message);
-      }
-    }
-  }
-
   render() {
+    function overlay() {
+      var el = document.getElementById("myModal");
+      el.style.visibility =
+        el.style.visibility == "visible" ? "hidden" : "visible";
+    }
+
     return (
       <div className="Subscribe">
-        <div id="mc_embed_signup">
-          <form
-            action="https://mentat.us18.list-manage.com/subscribe/post?u=39a9a73f63305a63b0633f092&amp;id=e5bd622ff3"
-            method="post"
-            id="mc-embedded-subscribe-form"
-            name="mc-embedded-subscribe-form"
-            class="validate"
-            target="_blank"
-            novalidate
-          >
-            <div id="mc_embed_signup_scroll">
-              <input
-                type="email"
-                name="EMAIL"
-                class="email"
-                id="mce-EMAIL"
-                placeholder="Enter your email address..."
-                required
-              />
-              <div class="clear">
-                <input
-                  type="submit"
-                  value="Subscribe"
-                  name="subscribe"
-                  id="mc-embedded-subscribe"
-                  class="button"
-                />
-              </div>
-            </div>
-          </form>
+        <button onClick={overlay}>Subscribe</button>
+        <div id="myModal">
+          <button id="closeButton" onClick={overlay}>
+            Close
+          </button>
+          <iframe
+            src="https://mentat.us18.list-manage.com/subscribe?u=39a9a73f63305a63b0633f092&id=e5bd622ff3"
+            allowfullscreen
+            width="100%"
+            height="100%"
+          />
         </div>
       </div>
     );
